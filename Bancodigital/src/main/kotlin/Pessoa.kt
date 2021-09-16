c7e448a251502fed5e43eb5acc1feefd5f94399d
Carloxslip <90391286+Carloxslip@users.noreply.github.com>
2021-09-16 18:55:10 -0300

    Add files via upload
    Criação do projeto e primeira classe

---
+++ Bancodigital/src/main/kotlin/Pessoa.kt
@@ -0,0 +1,12 @@
+class Pessoa {
+    var nome: String = "Antonio"
+    var cpf: String = "123.456.321-97"
+
+}
+
+fun main() {
+    val antonio = Pessoa()
+    println(antonio.nome)
+    println(antonio.cpf)
+
+}
